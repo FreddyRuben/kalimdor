4e8bf9f0433c44228d2e6bf0befa96cfa0e359ca
Freddy Ruben <freddyruben@outlook.com>
2019-09-16 00:43:07 -0500

    small modifications of code line

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb://localhost:27017/kalimdor'
 
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(db => console.log('db is connected'))
+    .then(() => console.log('db is connected'))
     .catch(err => console.error(err));
 
 module.exports = mongoose;
