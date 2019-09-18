e10522ab237cc2f7f3e5dd4d74bce54c2e7a8496
Freddy Ruben <freddyruben@outlook.com>
2019-09-18 17:45:59 -0500

    Added update method.

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -15,9 +15,7 @@ movieCtr.getMovie = async (req,res) => {
 }
 
 movieCtr.createMovies = async (req, res) => {
-
-    console.log(req.file.path);
-
+    console.log(req.body);
     const newMovie = new Movie({
         title: req.body.title,
         date: req.body.date,
@@ -25,14 +23,25 @@ movieCtr.createMovies = async (req, res) => {
         description: req.body.description,
         videoPath: req.file.path
     });
+    console.log(req.file.path);
     const movie = new Movie(newMovie)
     await movie.save()
-    return res.json({message: "Saved Succesfully", status: movie});
+    return res.json({message: "Movie Saved Succesfully", movie: movie});
+}
+
+movieCtr.updateMovie = async(req, res) => {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        date: req.body.date,
+        gender: req.body.gender,
+        description: req.body.description
+    }, {new: true});
+    res.json({message: "Movie Succesfully Updated", movie: movie});
 }
 
 movieCtr.deleteMovie = async (req,res)=>{
     await Movie.findByIdAndDelete(req.params.id);
-    res.json({"status":"movie successfully removed .."});
+    res.json({"status":"Movie Successfully Removed"});
 
 }
 
